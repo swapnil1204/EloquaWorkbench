32332b9a3c6305046e8f25036d14d00274423ae8
DT <GGITS_2020_INTERNS@hotmail.com>
2020-06-02 19:30:21 +0530

    DT-- fir deployment

--- Workbench/controllers/mainController.js
+++ Workbench/controllers/mainController.js
@@ -9,8 +9,11 @@ workbench
         main.init();
 
         $scope.Query_function = function() {
+            document.getElementById("Query_Ui").style.borderBottom = "solid 2px red";
             document.getElementById("Query").style.display = "block";
             document.getElementById("Api_div").style.display = "none";
+            document.getElementById("Apiform").style.borderBottom = "";
+
 
 
         }
@@ -18,10 +21,12 @@ workbench
         $scope.Apidiv_function = function() {
             document.getElementById("Api_div").style.display = "block";
             document.getElementById("Query").style.display = "none";
+            document.getElementById("Apiform").style.borderBottom = "solid 2px red";
+            document.getElementById("Query_Ui").style.borderBottom = "";
 
         }
 
-        $scope.getdropdown = function() {
+        $scope.getdropdown = function(id) {
             console.log("clicked");
             $http({
                     url: "http://localhost:4000/api/REST/1.0/data/OracleEloquaApis",
@@ -32,6 +37,41 @@ workbench
                 }).then(function(response) {
                     console.log(response.data);
                     $scope.Eloqua_api = response.data;
+
+                })
+                .catch(function(error) {
+                    alert("Error" + error)
+                });
+
+
+        }
+        $scope.getaccountdropdown = function(id) {
+            console.log(id);
+
+            var URL; { URL = "http://localhost:4000/api/REST/1.0/data/AccountApis"; }
+            console.log(URL);
+            $http({
+                    url: URL,
+                    method: "GET",
+                    data: {}
+                }).then(function(response) {
+                    console.log(response.data);
+                    $scope.Accounts_api = response.data;
+                })
+                .catch(function(error) {
+                    alert("Error" + error)
+                });
+        }
+
+        $scope.CreateAnAccount = function() {
+            $http({
+                    url: "http://localhost:4000/api/REST/1.0/data/CreateAccountApis",
+                    method: "GET",
+                    data: {}
+                }).then(function(response) {
+                    console.log(response.data);
+                    $scope.Create_api = response.data.parameter;
+                    $scope.Create_head = response.data.elements;
 
                 })
                 .catch(function(error) {
